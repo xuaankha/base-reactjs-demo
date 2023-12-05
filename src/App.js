f10bf6f08d678c6f1ece94785420a0e00e479cdb
Xuan Khaa <khaaxuannguyen@gmail.com>
2023-12-05 23:55:33 +0700

    vd16

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import MyComponent from "./components/MyComponents";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Kha Xuan Nguyen</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {" "}
+      Xuan Kha Nguyen
+      <MyComponent></MyComponent>
     </div>
   );
 }
