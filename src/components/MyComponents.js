d29e4efa6453bd68181d8ba4814d713233945efb
Xuan Khaa <khaaxuannguyen@gmail.com>
2023-12-20 00:17:32 +0700

    update

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -6,21 +6,29 @@ class MyComponent extends React.Component {
     address: "TP HCM",
     age: 26,
   };
-  handleClick(event) {
+  handleClick = (event) => {
     // console.log("Click me");
     console.log("My name is", this.state.name);
-  }
+    console.log("Random", Math.floor(Math.random() * 100) + 1);
+    this.setState({
+      name: "Kha Pro",
+    });
+  };
   handleonMoverOver(event) {
-    console.log(event);
+    // console.log(event);
   }
   render() {
     return (
       <div>
-        My name is {this.state.name} and I'm from {this.state.address}
+        My name is {this.state.name} and I'm from {this.state.age}
         <button onMouseOver={this.handleonMoverOver}>
           Click me onMouseOver
         </button>
         <button onClick={this.handleClick}>Click me onClick</button>
+        {/* có thể gọi arrow function vào this.handleClick luon
+        (event) => {
+            this.handleClick(event);
+          } */}
       </div>
     );
   }
